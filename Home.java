be20a132b25e1952cb02ea2cbaa151e2a0351f30
vinayagam <vinayagam7472@gmail.com>
2026-06-02 21:49:25 +0530

    add code print num

--- Home.java
+++ Home.java
@@ -1,17 +1,28 @@
 
-public class Home
-{
-//static refers to class specific information
-static String name = "Kathirvel";
-static int num = 15;
+// public class Home
+// {
+// //static refers to class specific information
+// static String name = "Kathirvel";
+// static int num = 15;
 
-//non-static variable / instance variable / object variable
-String name2 = "balamurugan";
-int age = 25;
+// //non-static variable / instance variable / object variable
+// String name2 = "balamurugan";
+// int age = 25;
 
-public static void main(String[] args)
-{
-    System.out.println("Hello World");
-    System.out.println(Home.name);
-}
+// public static void main(String[] args)
+// {
+//     System.out.println("Hello World");
+//     System.out.println(Home.name);
+// }
+// }
+
+public class Home {
+    public static void main(String[] args) {
+        int start = 1;
+
+        while (start <= 5) {
+            System.out.print(start + " "); // same line output
+            start = start + 1;
+        }
+    }
 }
