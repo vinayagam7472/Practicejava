b4f7aacc5d332a96fbb51d465912c4888db78a0e
vinayagam <vinayagam7472@gmail.com>
2026-06-02 18:21:51 +0530

    practice constructor

--- Trail.java
+++ Trail.java
@@ -1,13 +1,27 @@
 public class Trail
 {
-    static int num=21;
-     void shop(){
-        System.out.println("biscuit");
+    
+    String product ;
+    int rate;
+
+    Trail(String prod_name,int price){
+        product=prod_name;
+        rate=price;
+        
     }
     public static void main(String[] args) {
-        Trail item=new Trail();
-        item.shop();
-        
-        System.err.println(Trail.num);
+        Trail item1=new Trail("chocalate ",10);
+        Trail item2=new Trail("biscuit ",100);
+        System.out.println(item1.product);
+        System.out.println(item1.rate);
+
+        System.out.println(item2.product);
+       
     }
+
+
+
+    // public static void main (String[] args){
+    //     System.out.println();
+    // }
 }
