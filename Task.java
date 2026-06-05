7f541716170cdf66af3706ecacf512bd43461efd
vinayagam <vinayagam7472@gmail.com>
2026-06-05 09:19:34 +0530

    add print

--- Task.java
+++ Task.java
@@ -1,10 +1,7 @@
-public class Task {
-    
-    public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                System.out.println(i);
-            }
-        }
-    }
+public class Task{
+
+
+public static void main(String[] args){
+    System.out.println("vinayagam");
+}
 }
