43485c63623403a7113ff387881f0cb1f131bec7
vinayagam <vinayagam7472@gmail.com>
2026-06-05 11:22:22 +0530

    added shop package

--- Supermarket.java
+++ Supermarket.java
@@ -6,11 +6,11 @@ public class Supermarket {
      
 public static void main(String[] args){
     Supermarket item=new Supermarket();
-    item.product("vin",21);
+    item.product();
    
    
 }
- public void product(String name,int a){
-        System.out.println(name);
+ public void product(){
+        System.out.println("calling");
     }
 }
